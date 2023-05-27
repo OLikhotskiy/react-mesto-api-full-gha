263151f90a854a2d2cd0ac5f3a7766a8adaf7783
Oleg Likhotskiy <lihotsky777@gmail.com>
2023-05-28 00:11:24 +0200

    ПР15 cookies3

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,7 @@ const errorsCelebrate = require('celebrate').errors;
 const errorHandlers = require('./utils/handlers');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
-
+const cors = require('cors');
 const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
@@ -24,14 +23,8 @@ app.use(cors({
   origin: [
     'https://project-mesto.nomoredomains.rocks',
     'http://project-mesto.nomoredomains.rocks',
-    'http://project-mesto.nomoredomains.rock/signin',
-    'https://api.project-mesto.nomoredomains.rocks/users/me',
-    'https://api.project-mesto.nomoredomains.rocks/cards',
-    'https://api.project-mesto.nomoredomains.rocks/signup',
     'http://localhost:3000',
     'http://localhost:3001',
-    'http://localhost:4000',
-    'https://51.250.87.238',
     'http://51.250.87.238',
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
@@ -40,6 +33,7 @@ app.use(cors({
   optionsSuccessStatus: 204,
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
