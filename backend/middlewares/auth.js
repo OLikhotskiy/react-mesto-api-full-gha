f22ded77bca9ebeab48ce5c90dea4b9943fa4392
Oleg Likhotskiy <lihotsky777@gmail.com>
2023-05-27 19:25:51 +0200

    ПР15 auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,10 +4,11 @@ const { NODE_ENV, JWT_SECRET_KEY } = process.env;
 const Notauthorized = require('../errors/Notauthorized');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
-  if (!token) {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     return next(new Notauthorized('Авторизируйтесь!'));
   }
+  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET_KEY : 'secret-key');
