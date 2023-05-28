7973fd68a481712e543d5c74609b639af9676df3
Oleg Likhotskiy <lihotsky777@gmail.com>
2023-05-28 13:58:34 +0200

    ПР15 fix:cards

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -2,31 +2,24 @@ const Forbidden = require('../errors/Forbidden');
 
 const Card = require('../models/card');
 const { CREATED_CODE } = require('../utils/constants');
-
+// GET ALL CARDS
 module.exports.getCards = (req, res, next) => {
   Card.find({})
-    .populate([
-      { path: 'owner', model: 'user' },
-      { path: 'likes', model: 'user' },
-    ])
-    .then((card) => {
-      res.send({ data: card });
-    })
+    .populate(['owner', 'likes'])
+    .then((cards) => res.send(cards))
     .catch(next);
 };
-
+// NEW CARD
 module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
-  const owner = req.user;
-  Card.create({ name, link, owner })
-    .then((card) => card.populate('owner'))
-    .then((card) => res.status(CREATED_CODE).send({ data: card }))
+  const id = req.user._id;
+  Card.create({ name, link, owner: id })
+    .then((card) => res.status(CREATED_CODE).send(card))
     .catch(next);
 };
-
+// DELETE CARD
 module.exports.deleteCard = (req, res, next) => {
   const _id = req.params.cardId;
-
   Card.findOne({ _id })
     .orFail()
     .then((card) => {
@@ -40,24 +33,21 @@ module.exports.deleteCard = (req, res, next) => {
     })
     .catch(next);
 };
-
-const updateCardLikes = (req, res, updateData, next) => {
+// CARD LIKES
+function updateCardLikes(req, res, updateData, next) {
   Card.findByIdAndUpdate(req.params.cardId, updateData, { new: true })
     .orFail()
-    .populate([
-      { path: 'owner', model: 'user' },
-      { path: 'likes', model: 'user' },
-    ])
-    .then((card) => res.send({ data: card }))
+    .populate(['owner', 'likes'])
+    .then((card) => res.send(card))
     .catch(next);
-};
-
+}
+// LIKE CARD
 module.exports.likeCard = (req, res, next) => {
   const owner = req.user._id;
   const newData = { $addToSet: { likes: owner } };
   updateCardLikes(req, res, newData, next);
 };
-
+// DISLIKE CARD
 module.exports.dislikeCard = (req, res, next) => {
   const owner = req.user._id;
   const newData = { $pull: { likes: owner } };
